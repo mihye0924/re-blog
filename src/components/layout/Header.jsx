ffeb8cd5eac609b246d82e2628acf22aaa903b13
chomihye <mihye0924@freeive.com>
2023-09-18 10:20:03 +0900

    #컴포넌트 분리

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,10 +3,14 @@ import Button from '@/components/common/Button.jsx'
 
 const Header = () => {
     return (
-        <div className={header.header_wrap}>
+        <header className={header.header_wrap}>
             <div className={header.header}>
                 <div className={header.header_logo}>
-                    <img src="/images/layout/logo.png" alt="로고"/>
+                    <h1>
+                        <a href='#'>
+                            <img src="/images/layout/logo.png" alt="로고" />
+                        </a>
+                    </h1>
                     <div className={header.header_search}>
                         <input type="text" placeholder="찾고싶은 주제 혹은 닉네임을 입력하세요"/>
                     </div>
@@ -18,7 +22,7 @@ const Header = () => {
                     <Button name="로그인" color="white" />
                 </div>
             </div>
-        </div>
+        </header>
     );
 };
 
---
+++ src/components/layout/RightLayout.jsx
@@ -0,0 +1,8 @@
+import right from '@/assets/scss/layout/right.module.scss'
+const RightLayout = () => {
+  return (
+    <article className={right.right_wrap}> 
+    </article>
+  )
+}
+export default RightLayout
