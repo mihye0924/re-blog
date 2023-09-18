7b7ca7620c93ec105021fa054b4dc14be2eada46
chomihye <mihye0924@freeive.com>
2023-09-18 15:30:16 +0900

    # left, right 컴포넌트 수정

--- src/components/layout/LeftLayout.jsx
+++ src/components/layout/LeftLayout.jsx
@@ -1,54 +1,52 @@
-
 // import { useEffect } from 'react'
 import left from '@/assets/scss/layout/left.module.scss'
 import { popularList } from '@/api/popularList.jsx' 
 import { newsList } from '@/api/newsList.jsx' 
 import List from '@/components/common/List.jsx'    
 import { Link } from 'react-router-dom'
-const LeftLayout = () => {
+const leftLayout = () => {
   return (
-    
-    <aside className={left.left_wrap}>
-    <div className={left.left_wrap_div}> 
-      <List list={popularList} keys={"popular"}/>
-      <List list={newsList} keys={"news"}/>
-      <div className={left.left_download}>
-        <div className={left.left_download_title}> 
-            <span>앱 다운로드</span>
-        </div>
-        <ul className={left.left_download_ul}>
-          <li>
-            <button>
-              <img src='/images/content/download/appstore.png' alt='애플스토어' />
-              <span>App Store</span>
-            </button>
-          </li>
-          <li> 
-            <button>
-              <img src='/images/content/download/googleplay.png' alt='구글스토어' />
-              <span>Google Play</span>
-            </button>
-          </li>
-        </ul>
+  <aside className={left.left_wrap}>
+  <div className={left.left_wrap_div}> 
+    <List list={popularList} keys={"popular"}/>
+    <List list={newsList} keys={"news"}/>
+    <div className={left.left_download}>
+      <div className={left.left_download_title}> 
+          <span>앱 다운로드</span>
       </div>
-      <div className={left.left_footer}>
-        <ul className={left.left_info}>
-          <li>
-            <Link to="/">이용약관</Link>
-          </li>
-          <li>
-            <Link to="/">개인정보처리방침</Link>
-          </li>
-          <li>
-            <Link to="/">광고정보</Link>
-          </li>
-        </ul>
-        <div className={left.left_copyright}>
-          © 2021 owwners, All rights reserved.
-          </div>
-      </div>
+      <ul className={left.left_download_ul}>
+        <li>
+          <button>
+            <img src='/images/content/download/appstore.png' alt='애플스토어' />
+            <span>App Store</span>
+          </button>
+        </li>
+        <li> 
+          <button>
+            <img src='/images/content/download/googleplay.png' alt='구글스토어' />
+            <span>Google Play</span>
+          </button>
+        </li>
+      </ul>
     </div>
-  </aside>
+    <div className={left.left_footer}>
+      <ul className={left.left_info}>
+        <li>
+          <Link to="/">이용약관</Link>
+        </li>
+        <li>
+          <Link to="/">개인정보처리방침</Link>
+        </li>
+        <li>
+          <Link to="/">광고정보</Link>
+        </li>
+      </ul>
+      <div className={left.left_copyright}>
+        © 2021 owwners, All rights reserved.
+        </div>
+    </div>
+  </div>
+</aside>
   )
 }
-export default LeftLayout
+export default leftLayout
--- src/components/layout/RightLayout.jsx
+++ src/components/layout/RightLayout.jsx
@@ -1,9 +1,13 @@
+
+
 import right from '@/assets/scss/layout/right.module.scss'
 const RightLayout = () => {
   return (
+    
     <article className={right.right_wrap}> 
        
     </article>
+    
   )
 }
 export default RightLayout
