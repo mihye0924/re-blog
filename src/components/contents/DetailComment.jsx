60a4fddc7c53ced201a21af57a7e8805d3d0caa4
FEEL-2012309\user <dndgml99@freeive.com>
2023-10-05 01:32:58 +0900

    list.jsx 63번 째 줄 에러 납니다.

--- src/components/contents/DetailComment.jsx
+++ src/components/contents/DetailComment.jsx
@@ -1,32 +1,26 @@
 import detailComment from '@/assets/scss/contents/detailComment.module.scss'
 import { Context } from '@/context/Context';
-import data from '@/api/list'  
 import { useContext, useEffect, useMemo, useState } from 'react';
 function DetailComment({commentLength}) {
   const {newProfile} = useContext(Context);   
   const [commentValue, setCommentValue] = useState()
-  const [commentList, setCommentList] = useState(data)
+  const [commentList, setCommentList] = useState(JSON.parse(window.localStorage.getItem("list")))
   const handleCommentPush = useMemo(() => {
-    return (()=> {
-      setCommentList([...commentList], commentList.push(
-        {
-          id: commentList.length + 1,
-          img: newProfile.img,
-          name: newProfile.name,
-          nickname: '별명은 어떻게 하지?',
-          time: '1',
-          text: commentValue
-        }
-      ))
+    return (() => {
+      commentList.forEach(item => {
+        item.comment.push(
+          {
+            id: commentList.length + 1,
+            img: newProfile.img,
+            name: newProfile.name,
+            nickname: '별명은 어떻게 하지?',
+            time: '1',
+            text: commentValue
+          }
+        )
+      })
     })
   }, [])
-  useEffect(() => {
-    data.forEach((item) => {
-      console.log(item)
-      item.comment = []
-    })
-    handleCommentPush()
-  },[])
   return (
     <article className={detailComment.detailComment}>
       <div className={detailComment.detailComment_wrap}>
@@ -46,30 +40,31 @@ function DetailComment({commentLength}) {
         </div>
         <ul className={detailComment.detailComment_comment}>
           {
-            commentList.text !== '' ?
             commentList.map(item => {
-              return (
-                <li key={item.id} className={detailComment.detailComment_comment_list}>
-                  <div className={detailComment.detailComment_comment_list_thumb}>
-                    <img src={item.img ? item.img  : '/images/common/profile.png'} alt='' />
-                  </div>
-                  <div className={detailComment.detailComment_comment_list_item}>
-                    <div className={detailComment.detailComment_comment_list_item_wrap}>
-                      <div className={detailComment.detailComment_comment_list_item_head}>
-                        <p><span className={detailComment.detailComment_comment_list_item_head_name}>{item.name}</span><span>{item.nickname}</span></p>
-                      </div>
-                      <div className={detailComment.detailComment_comment_list_item_content}>
-                        {item.text}
-                      </div>
-                      <div className={detailComment.detailComment_comment_list_item_bottom}>
-                        <span className={detailComment.detailComment_comment_list_item_bottom_time}>{item.time}분</span><button>답글 달기</button>
+              item.comment.map(comment => {
+                return(
+                  <li key={comment.id} className={detailComment.detailComment_comment_list}>
+                    sdada
+                    <div className={detailComment.detailComment_comment_list_thumb}>
+                      <img src={comment.img ? comment.img  : '/images/common/profile.png'} alt='' />
+                    </div>
+                    <div className={detailComment.detailComment_comment_list_item}>
+                      <div className={detailComment.detailComment_comment_list_item_wrap}>
+                        <div className={detailComment.detailComment_comment_list_item_head}>
+                          <p><span className={detailComment.detailComment_comment_list_item_head_name}>{comment.name}</span><span>{comment.nickname}</span></p>
+                        </div>
+                        <div className={detailComment.detailComment_comment_list_item_content}>
+                          {comment.text}
+                        </div>
+                        <div className={detailComment.detailComment_comment_list_item_bottom}>
+                          <span className={detailComment.detailComment_comment_list_item_bottom_time}>{comment.time}분</span><button>답글 달기</button>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </li>
-              )
+                  </li>
+                )
+              })
             })
-            : false
           }
         </ul>
       </div>
