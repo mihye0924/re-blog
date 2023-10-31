320aab85e62484b2933a7386c2d0c9b7eb275ed0
mihye0924 <mihye0924@naver.com>
2023-10-31 12:07:17 +0900

    배포 수정

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 const __dirname = path.resolve();
 
-
-// https://vitejs.dev/config/
+ 
 export default defineConfig({
   base: "/re-blog",
   plugins: [react()],
