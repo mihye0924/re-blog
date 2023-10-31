62056aadb4630d906ed4af1bc86721cedb74dfbd
mihye0924 <mihye0924@naver.com>
2023-10-31 12:51:53 +0900

    라우터 체크

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import '@/assets/scss/index.scss'
-import {  BrowserRouter as Router  } from 'react-router-dom';
+import {  HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode> 
-    <Router basename={import.meta.env.BASE_URL}>
+    <HashRouter  basename={import.meta.env.BASE_URL}>
       <App />
-    </Router>
+    </HashRouter>
   // </React.StrictMode>
 )
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -6,10 +6,6 @@ import DetailContent from '@/components/contents/DetailContent'
 import DetailComment from '@/components/contents/DetailComment'  
 
 function Detail() {
-  useEffect(() => {
-    window.location.pathname = location.pathname
-    console.log(location.pathname)
-  },[])
   return ( 
       <article className={left.left_wrap}> 
         <DetailHeader />
