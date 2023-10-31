9b1b26735720220a1dd0bdfc908d033aeb58008f
mihye0924 <mihye0924@naver.com>
2023-10-31 12:43:29 +0900

    라우터 체크

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,10 +1,15 @@
 
+import  { useEffect } from 'react';
 import left from '@/assets/scss/layout/left.module.scss'
 import DetailHeader from '@/components/contents/DetailHeader'
 import DetailContent from '@/components/contents/DetailContent'
 import DetailComment from '@/components/contents/DetailComment'  
 
 function Detail() {
+  useEffect(() => {
+    
+    console.log(location.pathname)
+  },[])
   return ( 
       <article className={left.left_wrap}> 
         <DetailHeader />
