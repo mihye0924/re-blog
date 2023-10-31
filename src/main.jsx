dd4b652fbe9511d6acd8c32a0617dc57fd397651
mihye0924 <mihye0924@naver.com>
2023-10-31 12:54:14 +0900

    라우터 체크

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import '@/assets/scss/index.scss'
-import {  HashRouter } from 'react-router-dom';
+import {  BrowserRouter as Router  } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode> 
